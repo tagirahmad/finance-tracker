882a83a54f8a1ce968f0df04f0ee6f9663c7bcb2
tagirahmad <tagirbpl@gmail.com>
2021-01-30 12:24:21 +0300

    Implement search method for friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
     return false unless stock
-    
+
     stocks.where(id: stock.id).exists?
   end
 
@@ -26,6 +26,31 @@ class User < ApplicationRecord
 
   def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
+
     'Anonymous'
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
   end
 end
